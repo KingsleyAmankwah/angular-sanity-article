31d458111cb2eac1f3789cce03704ce8e22e5edd
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-04-18 09:12:25 +0000

    feat: setup post feature

---
+++ angular/src/app/posts/posts.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, signal } from '@angular/core';
+import { SanityService } from '../service/sanity.service';
+import { Router } from '@angular/router';
+import { Post } from '../models/post.model';
+import { BlockContentToTextPipe } from '../pipe/block-content-to-text.pipe';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from '../loader/loader.component';
+
+@Component({
+  selector: 'app-posts',
+  standalone: true,
+  imports: [BlockContentToTextPipe, CommonModule, LoaderComponent],
+  templateUrl: './posts.component.html',
+})
+export class PostsComponent {
+  private readonly sanityService = inject(SanityService);
+  private readonly router = inject(Router);
+
+  protected posts = signal<Post[]>([]);
+  protected isLoading = signal(true);
+
+  ngOnInit() {
+    this.loadPosts();
+  }
+
+  protected async loadPosts() {
+    const fetchPosts = await this.sanityService.getPost();
+    this.posts.set(fetchPosts);
+    this.isLoading.set(false);
+  }
+
+  protected navigateToPostBySlug(slug: string) {
+    this.router.navigate(['post', slug]);
+  }
+}
