be5f07c159f436b67210f4a15b65c55856801479
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-04-18 08:54:48 +0000

    feat: setup sanity service

---
+++ angular/src/app/service/sanity.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { client } from '../config/sanity-client';
+import { getAllPostsQuery, getPostBySlugQuery } from '../queries/posts.groq';
+import { Post } from '../models/post.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SanityService {
+  async getPost() {
+    try {
+      return await client.fetch(getAllPostsQuery);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getPostBySlug(slug: string): Promise<Post | null> {
+    try {
+      const query = getPostBySlugQuery(slug);
+      return await client.fetch(query);
+    } catch (error) {
+      console.error(`Error fetching post with slug "${slug}":`, error);
+      throw error;
+    }
+  }
+}
