4cf9a31842e540391210668a44b066c4c3ee9a5e
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-04-18 08:52:24 +0000

    feat: setup routes for post

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'posts',
+    loadComponent: () =>
+      import('./posts/posts.component').then((c) => c.PostsComponent),
+  },
+
+  {
+    path: 'post/:slug',
+    loadComponent: () =>
+      import('./view-post/view-post.component').then(
+        (c) => c.ViewPostComponent
+      ),
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/posts',
+  },
+
+  {
+    path: '**',
+    redirectTo: '/posts',
+    pathMatch: 'full',
+  },
+];
